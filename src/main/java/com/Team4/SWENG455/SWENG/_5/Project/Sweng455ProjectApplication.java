d9f6e8950a1f6f53743ab4bb527aa4ddbcc1743d
ChibuikeOD <90789630+ChibuikeOD@users.noreply.github.com>
2024-07-30 16:39:45 -0400

    Initialized Spring project
    -Will need maven installed to build the project.
    -Also need to set up Java and maven environment variables

---
+++ src/main/java/com/Team4/SWENG455/SWENG/_5/Project/Sweng455ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Team4.SWENG455.SWENG._5.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sweng455ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sweng455ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Team4/SWENG455/SWENG/_5/Project/Sweng455ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team4.SWENG455.SWENG._5.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sweng455ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
