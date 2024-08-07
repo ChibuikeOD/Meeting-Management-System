432260d9418e3ad8e1166fb9940983d06bdade4b
ChibuikeOD <90789630+ChibuikeOD@users.noreply.github.com>
2024-08-06 22:07:04 -0400

    Fixed error that did not let the app run
    -The database was not yet configured so the app would not build. I disabled the DataSourceAutoConfiguration for now until we set up our database

--- src/main/java/com/Team4/SWENG455/SWENG/_5/Project/Sweng455ProjectApplication.java
+++ src/main/java/com/Team4/SWENG455/SWENG/_5/Project/Sweng455ProjectApplication.java
@@ -2,8 +2,9 @@ package com.Team4.SWENG455.SWENG._5.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Sweng455ProjectApplication {
 
 	public static void main(String[] args) {
